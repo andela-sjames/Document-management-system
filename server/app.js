d2708bcce049647fa20dd6c7616da71fc0e882be
andela-sjames <samuel.james@andela.com>
2017-02-24 03:02:35 -0500

    add basic server and sequelize setting

--- server/app.js
+++ server/app.js
@@ -9,12 +9,36 @@ const app = express();
 app.use(logger('dev'));
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
+// this will let us get the data from a POST
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+const router = express.Router(); 
+app.use('/api', router);
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
+
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('something is about to happen')
+    next(); // make user go to the next route and don't stop here.
+});
+
+
+router.get('*', (req, res) => res.status(200).send({
+  message :'Hello you have successfully made it here......',
 }));
 
-module.exports = app;
+const port = parseInt(process.env.PORT, 10) || 8080;
+app.set('port', port);
+
+app.listen(port);
+console.log(`server started on port ${port}`);
+
+module.exports = router;
