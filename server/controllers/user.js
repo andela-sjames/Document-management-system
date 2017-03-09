2ae5bf245b795281caa270c0d2d26505aaea848e
andela-sjames <samuel.james@andela.com>
2017-03-08 23:22:53 -0500

    create user.js file

---
+++ server/controllers/user.js
@@ -0,0 +1,11 @@
+const User = require('..models').User;
+
+module.exports = {
+    create:(req, res) => {
+        // username: req.body.username || '',
+        // password: req.body.password || '',
+
+        
+
+    }
+}
